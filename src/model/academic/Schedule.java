d08bf9524f08a934510035cddad7ad23fab3ea0d
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-04-14 17:13:57 +0500

    Update Schedule.java

--- src/model/academic/Schedule.java
+++ src/model/academic/Schedule.java
@@ -1,4 +1,11 @@
-package model.academic;
+package university.model.academic;
 
 public class Schedule {
+    private String date;
+    private String time;
+    private String room;
+
+    public void createSchedule() {}
+
+    public void updateSchedule() {}
 }
