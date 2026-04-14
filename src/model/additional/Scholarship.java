bf475e2abde949e3f9f8d150a046e9b076e7002c
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-04-14 17:15:04 +0500

    Update Scholarship.java

--- src/model/additional/Scholarship.java
+++ src/model/additional/Scholarship.java
@@ -1,4 +1,8 @@
-package model.additional;
+package university.model.additional;
+
+import university.model.people.Student;
 
 public class Scholarship {
+    private Student student;
+    private double amount;
 }
