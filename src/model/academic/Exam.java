2f7084aa7ffac6bafcd69d7a5ca8c8bbda70efc6
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-04-14 17:14:16 +0500

    Update Exam.java

--- src/model/academic/Exam.java
+++ src/model/academic/Exam.java
@@ -1,4 +1,16 @@
-package model.academic;
+package university.model.academic;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Exam {
+    private Course course;
+    private String date;
+    private List<Grade> grades = new ArrayList<>();
+
+    public void scheduleExam() {}
+
+    public void assignGrade(Grade grade) {
+        grades.add(grade);
+    }
 }
