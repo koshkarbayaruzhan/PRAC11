116b47e78884a2d720f197961f7b8a395288bb4c
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-04-14 17:10:24 +0500

    Update University.java

--- src/model/core/University.java
+++ src/model/core/University.java
@@ -1,4 +1,18 @@
-package model.core;
+package university.model.core;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class University {
+    private String name;
+    private String address;
+    private List<Faculty> faculties = new ArrayList<>();
+
+    public void addFaculty(Faculty faculty) {
+        faculties.add(faculty);
+    }
+
+    public void removeFaculty(Faculty faculty) {
+        faculties.remove(faculty);
+    }
 }
