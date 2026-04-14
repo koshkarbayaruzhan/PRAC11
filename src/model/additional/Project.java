6f19d763b040c409a6265ab80ae33eace9f9157b
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-04-14 17:15:45 +0500

    Update Project.java

--- src/model/additional/Project.java
+++ src/model/additional/Project.java
@@ -1,4 +1,12 @@
-package model.additional;
+package university.model.additional;
+
+import university.model.people.Student;
+import university.model.people.Teacher;
+
+import java.util.List;
 
 public class Project {
+    private String title;
+    private List<Student> participants;
+    private Teacher supervisor;
 }
