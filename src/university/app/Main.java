4bae35e88344c71bd3903ed98227ff8f00aaae39
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-04-14 17:16:29 +0500

    Update Main.java

--- src/university/app/Main.java
+++ src/university/app/Main.java
@@ -1,4 +1,44 @@
 package university.app;
 
+import university.model.core.*;
+import university.model.people.*;
+import university.model.academic.*;
+
 public class Main {
+    public static void main(String[] args) {
+
+        // Университет
+        University uni = new University();
+
+        // Факультет
+        Faculty faculty = new Faculty();
+        uni.addFaculty(faculty);
+
+        // Кафедра
+        Department dept = new Department();
+        faculty.addDepartment(dept);
+
+        // Препод
+        Teacher teacher = new Teacher();
+        dept.addTeacher(teacher);
+
+        // Студент
+        Student student = new Student();
+
+        // Курс
+        Course course = new Course();
+        course.addStudent(student);
+
+        // Экзамен
+        Exam exam = new Exam();
+        Grade grade = new Grade();
+
+        exam.assignGrade(grade);
+
+        // Пример действий
+        student.registerCourse(course);
+        teacher.conductLesson();
+
+        System.out.println("Система университета запущена");
+    }
 }
