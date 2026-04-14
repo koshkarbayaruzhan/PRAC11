baaf670d49cce09afb1ee8e99c29b840f3dbad92
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-04-14 17:14:36 +0500

    Update Grade.java

--- src/model/academic/Grade.java
+++ src/model/academic/Grade.java
@@ -1,4 +1,8 @@
-package model.academic;
+package university.model.academic;
+
+import university.model.people.Student;
 
 public class Grade {
+    private Student student;
+    private int value;
 }
