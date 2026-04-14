29ec41120feb5a358cdca7d44ccba5cf00d7fd96
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-04-14 17:13:08 +0500

    Update Admin.java

--- src/model/people/Admin.java
+++ src/model/people/Admin.java
@@ -1,4 +1,12 @@
-package model.people;
+package university.model.people;
 
-public class Admin {
+public class Admin extends User {
+
+    public void manageUsers() {
+        System.out.println("Managing users");
+    }
+
+    public void manageStructure() {
+        System.out.println("Managing university structure");
+    }
 }
