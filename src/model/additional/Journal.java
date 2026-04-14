88fbc9ca9d5f1b29980fb360f76669b323894c6a
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-04-14 17:16:00 +0500

    Update Journal.java

--- src/model/additional/Journal.java
+++ src/model/additional/Journal.java
@@ -1,4 +1,14 @@
-package model.additional;
+package university.model.additional;
+
+import university.model.academic.Grade;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Journal {
+    private List<Grade> grades = new ArrayList<>();
+
+    public void addGrade(Grade grade) {
+        grades.add(grade);
+    }
 }
