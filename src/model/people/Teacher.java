089d3eb9012e162197172f22f6d7a9b022ddd7b3
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-04-14 17:12:45 +0500

    Update Teacher.java

--- src/model/people/Teacher.java
+++ src/model/people/Teacher.java
@@ -1,4 +1,20 @@
-package model.people;
+package university.model.people;
 
-public class Teacher {
+import university.model.academic.Course;
+import university.model.academic.Grade;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Teacher extends User {
+    private String position;
+    private List<Course> courses = new ArrayList<>();
+
+    public void conductLesson() {
+        System.out.println("Lesson started");
+    }
+
+    public void assignGrade(Grade grade) {
+        System.out.println("Grade assigned");
+    }
 }
