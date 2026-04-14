e57f0371e6f4fb61425721590380a850aa0d281a
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-04-14 17:11:12 +0500

    Update Department.java

--- src/model/core/Department.java
+++ src/model/core/Department.java
@@ -1,4 +1,21 @@
-package model.core;
+package university.model.core;
+
+import university.model.people.Teacher;
+import university.model.academic.Course;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Department {
+    private String name;
+    private List<Teacher> teachers = new ArrayList<>();
+    private List<Course> courses = new ArrayList<>();
+
+    public void addTeacher(Teacher teacher) {
+        teachers.add(teacher);
+    }
+
+    public void removeTeacher(Teacher teacher) {
+        teachers.remove(teacher);
+    }
 }
