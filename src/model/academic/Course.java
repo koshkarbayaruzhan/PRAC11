328ee4883e4544ec7091a00dccb1438a03a262a0
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-04-14 17:13:31 +0500

    Update Course.java

--- src/model/academic/Course.java
+++ src/model/academic/Course.java
@@ -1,4 +1,23 @@
-package model.academic;
+package university.model.academic;
+
+import university.model.core.Department;
+import university.model.people.Student;
+import university.model.people.Teacher;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Course {
+    private String name;
+    private Department department;
+    private List<Student> students = new ArrayList<>();
+    private List<Teacher> teachers = new ArrayList<>();
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public void removeStudent(Student student) {
+        students.remove(student);
+    }
 }
