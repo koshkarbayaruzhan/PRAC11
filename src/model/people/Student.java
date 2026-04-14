49354318390f08e74f632e794697e11064be54b8
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-04-14 17:12:18 +0500

    Update Student.java

--- src/model/people/Student.java
+++ src/model/people/Student.java
@@ -1,4 +1,18 @@
-package model.people;
+package university.model.people;
 
-public class Student {
+import university.model.academic.Course;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student extends User {
+    private String studentId;
+    private List<Course> courses = new ArrayList<>();
+
+    public void registerCourse(Course course) {
+        courses.add(course);
+    }
+
+    public void viewGrades() {
+        System.out.println("Viewing grades...");
+    }
 }
